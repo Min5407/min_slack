3a2320b7e0085fc0db93e11be9259edfe8d9f4f4
Min <min@Mins-MacBook-Pro.local>
2020-09-02 00:55:18 +0900

    created chat header component and completed

---
+++ src/components/chat-header/chatHeader.component.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./chatHeader.style.scss";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import StarBorderRoundedIcon from "@material-ui/icons/StarBorderRounded";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
+
+const ChatHeader = () => {
+  return (
+    <div className="chat-header">
+      <div className="chat_header_name">
+        <FiberManualRecordIcon style={{ color: "green", fontSize: "1rem" }} />
+
+        <h4>Min</h4>
+        <StarBorderRoundedIcon style={{ fontSize: "1rem" }} />
+      </div>
+      <div>
+        <InfoOutlinedIcon />
+      </div>
+    </div>
+  );
+};
+
+export default ChatHeader;
--- src/components/chat/chat.component.jsx
+++ src/components/chat/chat.component.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./chat.style.scss";
+import ChatHeader from "../chat-header/chatHeader.component";
 import { useParams } from "react-router-dom";
 
 const Chat = () => {
   const { groupId } = useParams();
-  return <div className="chat">{groupId}</div>;
+  return (
+    <div className="chat">
+      <ChatHeader />
+    </div>
+  );
 };
 
 export default Chat;
